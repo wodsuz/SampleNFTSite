6e22fa47e55af903e2bfa0fe77675c9ed1a0177b
Ongun <ongun.demirag@gmail.com>
2024-01-10 20:09:19 +0100

    mobile responsiveness updated

--- src/components/DrawSvg.js
+++ src/components/DrawSvg.js
@@ -15,14 +15,19 @@ const VectorContainer = styled.div`
   height: 100%;
   overflow: hidden;
   svg {
+    display: inline-block;
     width: 100%;
     height: 100%;
+  }
+  @media (max-width: 48em) {
+    left: 1rem;
   }
 `;
 
 const Bounce = keyframes`
     from {transform: translateX(-50%) scale(0.5); }
     to {transform: translateX(-50%) scale(1); }
+   
 `;
 
 const Ball = styled.div`
@@ -35,6 +40,9 @@ const Ball = styled.div`
   border-radius: 50%;
   background-color: ${(props) => props.theme.text};
   animation: ${Bounce} 0.5s linear infinite alternate;
+  @media (max-width: 48em) {
+    left: 1rem;
+  }
 `;
 
 const DrawSvg = () => {
