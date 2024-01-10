e959709c183f999f31d73bb86788426e45ee38aa
Ongun <ongun.demirag@gmail.com>
2024-01-10 13:52:38 +0100

    mobile responsiveness updated

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -10,6 +10,9 @@ const LogoText = styled.h1`
   &:hover {
     transform: scale(1.1);
   }
+  @media (max-width: 64em) {
+    font-size: ${(props) => props.theme.fontxxl};
+  }
 `;
 
 const Logo = () => {
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Logo from "./Logo";
 import Button from "./Button";
@@ -16,6 +16,19 @@ const Navbar = styled.section`
   width: 85%;
   height: ${(props) => props.theme.navHeight};
   margin: 0 auto;
+
+  .mobile {
+    display: none;
+  }
+
+  @media (max-width: 64em) {
+    .desktop {
+      display: none;
+    }
+    .mobile {
+      display: inline-block;
+    }
+  }
 `;
 
 const Menu = styled.ul`
@@ -23,6 +36,27 @@ const Menu = styled.ul`
   justify-content: space-between;
   align-items: center;
   list-style: none;
+  @media (max-width: 64em) {
+    /* 1024px */
+    position: fixed;
+    top: ${(props) => props.theme.navHeight};
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100vw;
+
+    height: ${(props) => `calc(100vh - ${props.theme.navHeight})`};
+    z-index: 50;
+    background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.85)`};
+    backdrop-filter: blur(2px);
+
+    transform: ${(props) =>
+      props.click ? '"translateY(0)"}' : "translateY(100%)"};
+    transition: transform 0.3s ease;
+
+    flex-direction: column;
+    justify-content: center;
+  }
 `;
 
 const MenuItem = styled.li`
@@ -42,9 +76,62 @@ const MenuItem = styled.li`
   &:hover::after {
     width: 100%;
   }
+  @media (max-width: 64em) {
+    margin: 1rem 0;
+    &::after {
+      display: none;
+    }
+  }
+`;
+
+const HamburgerMenu = styled.span`
+  width: ${(props) => (props.click ? "2rem" : "1.5rem")};
+  height: 2px;
+  background: ${(props) => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  left: 50%;
+  transform: ${(props) =>
+    props.click
+      ? "translateX(-50%) rotate(90deg)"
+      : "translateX(-50%) rotate(0)"};
+
+  display: none;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  @media (max-width: 64em) {
+    /* 1024px */
+    display: flex;
+  }
+
+  &::before,
+  &::after {
+    content: " ";
+    width: ${(props) => (props.click ? "1rem" : "1.5rem")};
+    height: 2px;
+    right: ${(props) => (props.click ? "-2px" : "0")};
+    background: ${(props) => props.theme.text};
+    position: absolute;
+    transition: all 0.3s ease;
+  }
+
+  &::after {
+    top: ${(props) => (props.click ? "0.3rem" : "0.5rem")};
+    transform: ${(props) => (props.click ? "rotate(-40deg)" : "rotate(0)")};
+  }
+  &::before {
+    bottom: ${(props) => (props.click ? "0.3rem" : "0.5rem")};
+    transform: ${(props) => (props.click ? "rotate(40deg)" : "rotate(0)")};
+  }
 `;
 
 const Navigation = () => {
+  const [click, setClick] = useState(false);
+
   const scrollTo = (id) => {
     let element = document.getElementById(id);
     element.scrollIntoView({
@@ -52,22 +139,30 @@ const Navigation = () => {
       block: "start",
       inline: "nearest",
     });
+    setClick(!click);
   };
   return (
     <Section id="navigation">
       <Navbar>
         <Logo />
-
-        <Menu>
+        <HamburgerMenu click={click} onClick={() => setClick(!click)}>
+          &nbsp;
+        </HamburgerMenu>
+        <Menu click={click}>
           <MenuItem onClick={() => scrollTo("navigation")}>Home</MenuItem>
           <MenuItem onClick={() => scrollTo("about")}>About</MenuItem>
           <MenuItem onClick={() => scrollTo("roadmap")}>Roadmap</MenuItem>
           <MenuItem onClick={() => scrollTo("showcase")}>Showcase</MenuItem>
           <MenuItem onClick={() => scrollTo("team")}>Team</MenuItem>
           <MenuItem onClick={() => scrollTo("faq")}>Faq</MenuItem>
+          <div className="mobile">
+            <Button text="Connect Wallet" link="https://google.com" />
+          </div>
         </Menu>
 
-        <Button text="Connect Wallet" link="https://google.com" />
+        <div className="desktop">
+          <Button text="Connect Wallet" link="https://google.com" />
+        </div>
       </Navbar>
     </Section>
   );
